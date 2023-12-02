c621675052360f30f4f8c8a2631c6fd806e4da6b
Mian Raheel <raheelny@gmail.com>
2023-12-02 15:35:10 -0500

    adding lighthouse files

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Board from "./Board";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Board Game</h1>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
